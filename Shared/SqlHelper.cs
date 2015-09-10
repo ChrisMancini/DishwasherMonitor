4fd0e49152f18fe209330a97a0e5f912a26e78ce
batwomankt <katie.gould8@gmail.com>
2015-09-10 18:59:57 -0400

    don't want to lose my fun

---
+++ Shared/SqlHelper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System;
+using SQLite.Net;
+using SQLite.Net.Interop;
+using SQLite.Net.Platform.WinRT;
+
+namespace Shared
+{
+    public class SqlHelper
+    {
+        public SqlHelper()
+        {
+            _path = Path.Combine(Path.GetTempPath(), "dishwasher.db");
+            using (
+                var conn =
+                    new SQLiteConnection(new SQLitePlatformWinRT(), _path, SQLiteOpenFlags.Create | SQLiteOpenFlags.ReadWrite))
+            {
+                CreateTableIfNeeded<DishwasherRun>(conn);
+                conn.Commit();
+            }
+        }
+
+        readonly string _path;
+
+        public void AddDishwasherRunStart(DateTime start)
+        {
+            using (
+                var conn =
+                    new SQLiteConnection(new SQLitePlatformWinRT(), _path))
+            {
+                conn.Insert(new DishwasherRun
+                {
+                    StartDateTime = start
+                });
+
+                conn.Commit();
+            }
+        }
+
+        public void EndDishwasherRun(DateTime end)
+        {
+            using (
+                var conn =
+                    new SQLiteConnection(new SQLitePlatformWinRT(), _path))
+            {
+                var result =
+                    conn.Table<DishwasherRun>().Where(x => x.EndDateTime == DateTime.MinValue).OrderByDescending(x => x.StartDateTime).First();
+
+                if (result != null)
+                {
+                    result.EndDateTime = end;
+                    conn.Update(result);
+                    conn.Commit();
+                }
+            }
+        }
+
+        private static void CreateTableIfNeeded<T>(SQLiteConnection conn) where T :class
+        {
+            if (!conn.TableMappings.Any(table => typeof (T).IsAssignableFrom(table.MappedType)))
+            {
+                conn.CreateTable<T>(CreateFlags.AutoIncPK);
+            }
+        }
+    }
+}
